e7ab4f3044b25baa1ffffad77a66f7ccf5565191
unknown <ling.fei@D014.xian.superprogrommers.net>
2014-10-23 18:32:56 +0800

    111

--- public/plugin_assets/issue_evaluate/javascripts/issue_evaluate.js
+++ public/plugin_assets/issue_evaluate/javascripts/issue_evaluate.js
@@ -1,7 +1,7 @@
-function AddSuccess()
-{
-	alert("aaa");
-}
+// function AddSuccess()
+// {
+	// alert("aaa");
+// }
 
 $(document).ready(function() {
 	$('#star').raty({
@@ -9,3 +9,33 @@ $(document).ready(function() {
 		targetScore: '#issue_evaluates_score',
 	});
 });
+
+var Redmines = Redmines || {};
+
+Redmine.IssueEvaluate = Class.create({
+	initialize: function(button) {
+		this.button  = $(button);
+		// this.button.click($.proxy(this.readChecklist, this));
+		Event.observe(this.button, 'click', this.readChecklist.bindAsEventListener(this));
+	},
+
+	readChecklist: function(event) {
+		Event.stop(event);
+		alert("aaaaaaaaaaaaaaaa");
+	},
+	
+	addChecklist: function() {
+		alert("4444444");
+	},
+});	
+
+function observeIssueChecklistField(add_button) {
+	alert("111111111");
+	alert($(add_button));
+	issueChecklist = new Redmines.IssueEvaluate(add_button);
+	alert("222");
+}
+
+function createIssueChecklist() {
+	issueChecklist.addChecklist(); 
+}
